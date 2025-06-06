78eab550414517fa2a862df4177c15c8851f349d
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-06-06 19:53:21 +0530

    Chore: Write snapshot test for the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+// App.jsx
 
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
   return (
     <>
-      <h1>Testing react with vitest and RTL</h1>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,25 @@
-    import { describe, it, expect } from 'vitest';
-    import { render, screen } from '@testing-library/react';
-    import App from './App';
+// App.test.jsx
 
-    describe('App', () => {
-    it('renders headline', () => {
-        render(<App title="React" />);
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-        screen.debug();
+describe("App component", () => {
+  it("renders magnificent monkeys", () => {
+    // since screen does not have the container property, we'll destructure render to obtain a container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
 
-        // check if App components renders headline
-    });
-    });
+  it("renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+  });
+});
